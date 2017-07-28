67d8ad9ff34d99ed96eadf285d64289c6ffc4a63
Connor <30354840+connorcwell@users.noreply.github.com>
2017-07-28 12:01:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ lon = [float(item[2]) for item in array]
 a = [float(item[3]) for item in array]
 
 new_cord(lat, lon, a)
+
+#replace stamentoner with whatever tileset is best (custom ones can be made)
+m = folium.Map([48., 5.], tiles='stamentoner',control_scale = True, zoom_start=1)
+
+#play around with radius value to see which one is best, and gradient changes heatmap colors
+m.add_child(plugins.HeatMap(zip(lat, lon, a), radius = 25, gradient={.4: 'dodgerblue', .6: 'blue', 1: 'blue'}))
+
+print "created map"
+
+print "saving map..."
+#saves heatmap as an html file
+m.save('map.html')
+
