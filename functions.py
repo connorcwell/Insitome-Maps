454b23f26ed7109d5272225e7b283b98f6e9b935
Connor <30354840+connorcwell@users.noreply.github.com>
2017-08-07 12:13:53 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def intermediates(lat_1, lon_1, lat_2, lon_2, nb_points):
-    """"Return a list of nb_points equally spaced points
+    """Return a list of nb_points equally spaced points
     between p1 and p2"""
     # If we have 8 intermediate points, we have 8+1=9 spaces
     # between p1 and p2
@@ -11,10 +11,11 @@ def intermediates(lat_1, lon_1, lat_2, lon_2, nb_points):
     return [[lat_1 + i * x_spacing, lon_1 +  i * y_spacing]
             for i in range(1, nb_points+1)]
 
-def getPoints(lat_x, lon_y, a_z, smooth, nb_points):
+def getPoints(dataset, lat_x, lon_y, a_z, smooth, nb_points):
     if nb_points > 0:
         start = 0
         end=len(real)
+        new_cords = np.array([])
         while (start < end):
             lat_1=(real[start][0])
             lon_1=(real[start][1])
@@ -24,21 +25,23 @@ def getPoints(lat_x, lon_y, a_z, smooth, nb_points):
                 lon_2=(real[start2][1])
                 start2=start2+1
                 start3 = 0
-                new_cords = np.array([[1,2,3],[4,5,6]])
+
                 while (start3 < end):
+
                     interm = intermediates(lat_1,lon_1,lat_2,lon_2,nb_points)
                     interm = np.array(interm)
                     new_lat = [float(item[0]) for item in interm]
                     new_lon = [float(item[1]) for item in interm]
+
                     f = interpolate.Rbf(lat_x, lon_y, a_z, smooth=smooth)
 
                     new_a =  f(new_lat, new_lon)
                     new_set = (zip(new_lat,new_lon,new_a))
                     new_set_1 = np.array(new_set)
-                    new_cords = np.append(new_cords,new_set_1)
+                    new_cords =  np.append(new_cords,new_set_1)
                     start3=start3+1
             start=start+1
-
-        return new_cords
+            n = len(new_cords)/3
+        return np.reshape(new_cords, (n,3))
     else:
         print "Error in arg nb_points: Need to enter value greater than 0"
