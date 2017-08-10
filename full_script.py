a188a0a5faaeddab1f4e0b96a4a823265eb68106
Connor <30354840+connorcwell@users.noreply.github.com>
2017-08-10 17:11:21 -0500

    major structual changes

--- full_script.py
+++ full_script.py
@@ -1,9 +1,8 @@
-print "importing packages..."
+print "Loading..."
 import time
 start = time.time()
 import folium
 from folium import plugins
-from functions import *
 import pandas as pd
 import numpy as np
 from numpy import genfromtxt
@@ -11,20 +10,18 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import scipy
 from scipy import interpolate
-from scipy.interpolate import RectSphereBivariateSpline
-from pprint import pprint
-from scipy.interpolate import griddata
-from scipy.interpolate import interp1d
 from scipy.interpolate import Rbf
-import random
-from scipy import sparse
-from sklearn.neighbors import NearestNeighbors
 from math import cos, asin, sqrt
-import pprint
-from geopy.distance import vincenty
 from functools import partial
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+
+name = raw_input("What's your name? ")
+print "How are you %s?" % name
+how = raw_input("")
+
+if how != "Bad" and how != "Terrible" and how != "Not so Good" and how != "Eh":
+    print "Glad to hear that %s! Why don't we get on with the program then." % name
+else:
+    print "Sorry to hear that %s. Let's get on with the program." % name
 
 def getClosest(array, coord):
     dist=lambda s,d: (s[0]-d[0])**2+(s[1]-d[1])**2 #a little function which calculates the distance between two coordinates
@@ -34,7 +31,6 @@ def getClosest(array, coord):
     dat_new = np.delete(array, np.where(array == clos_new), axis=0)
     clos_new_1 = min(dat_new, key=partial(dist, coord))
     return np.concatenate((clos_new,clos_new_1))
-
 
 def distance(lon1, lat1, lon2, lat2):
     """
@@ -51,8 +47,6 @@ def distance(lon1, lat1, lon2, lat2):
     c = 2 * np.arcsin(np.sqrt(a))
     km = 6367 * c
     return km
-
-distance(21.565, 48.123, 100.765, 80.111)
 
 def closest(data, lat_data, lon_data, lat, lon):
     """
@@ -73,37 +67,28 @@ def intermediates(lat_1, lon_1, lat_2, lon_2, nb_points):
     return [[lat_1 + i * x_spacing, lon_1 +  i * y_spacing]
             for i in range(1, nb_points+1)]
 
-def getPoints(dataset, lat_x, lon_y, a_z, smooth, nb_points):
+def main(dataset, lat_x, lon_y, a_z, smooth, nb_points):
     """
     returns an array of midpoints (main function)
 
     """
     if nb_points > 0:
         start = 0
-        end=len(real)
+        end=len(dataset)
         new_cords = np.array([]) #creates empty numpy array for data to append to
         while (start < end):
-            lat_1=(real[start][0])
-            lon_1=(real[start][1])
+            lat_1=(dataset[start][0]) #loops through original cords
+            lon_1=(dataset[start][1])
             start2 = 0
             while (start2 < end):
-                lat_2=(real[start2][0])
-                lon_2=(real[start2][1])
+                lat_2=(dataset[start2][0]) #loops through again to get a pair of two cords
+                lon_2=(dataset[start2][1])
                 start2=start2+1
                 start3 = 0
 
                 while (start3 < end):
-                    clos = getClosest(dataset, (lat_1,lon_1))
-                    print clos
-                    lat_2 = clos[0]
-                    lon_2 = clos[1]
-                    lat_3 = clos[2]
-                    lon_3 = clos[3]
-                    interm_2 = intermediates(lat_1,lon_1,lat_3,lon_3,nb_points)
                     interm = intermediates(lat_1,lon_1,lat_2,lon_2,nb_points) #determines intermediates
                     interm = np.array(interm) #converts intermediates to numpy array
-                    interm_2 = np.array(interm_2)
-                    interm = np.concatenate((interm,interm_2))
                     new_lat = [float(item[0]) for item in interm] #seperates lat
                     new_lon = [float(item[1]) for item in interm] #seperates lon
                     f = interpolate.Rbf(lat_x, lon_y, a_z, smooth=smooth) #creates interpolation method
@@ -119,107 +104,99 @@ def getPoints(dataset, lat_x, lon_y, a_z, smooth, nb_points):
     else:
         print "Error in arg nb_points: Need to enter value greater than 0"
 
+def getData():
+    scope = ['https://spreadsheets.google.com/feeds']
 
-print "finished importing packages"
-print "logging into google drive..."
+    #retrieves credentials from Google Service Account to log in
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('data.json', scope)
+    gc = gspread.authorize(credentials)
 
-scope = ['https://spreadsheets.google.com/feeds']
+    #import population codes spreadsheet to pandas
+    pops = gc.open_by_key('1nP3mdssbSH9fftb8FHq9J8tFXHI0dqcARhRJ3mMpgGY')
+    p_sheet = pops.sheet1
+    pop = pd.DataFrame(p_sheet.get_all_records())
 
-#retrieves credentials from Google Service Account to log in
-credentials = ServiceAccountCredentials.from_json_keyfile_name('data.json', scope)
-gc = gspread.authorize(credentials)
+    #import cordinate spreadsheet to pandas
+    cords = gc.open_by_key('1c5ZbJPPIoIOdYjGtkXLjfFyUlZcN0DSgaSnkYevEdFw')
+    c_sheet = cords.sheet1
+    cord = pd.DataFrame(c_sheet.get_all_records())
 
-#import population codes spreadsheet to pandas
-pops = gc.open_by_key('1nP3mdssbSH9fftb8FHq9J8tFXHI0dqcARhRJ3mMpgGY')
-p_sheet = pops.sheet1
-pop = pd.DataFrame(p_sheet.get_all_records())
+    #import allele spreadsheet to pandas
+    alleles = gc.open_by_key('1SzXv3RQIim-eYhW4-OtVjveDd9Ph6QuXSTDTXqcN0ug')
+    a_sheet = alleles.sheet1
+    al = pd.DataFrame(a_sheet.get_all_records())
 
-#import cordinate spreadsheet to pandas
-cords = gc.open_by_key('1c5ZbJPPIoIOdYjGtkXLjfFyUlZcN0DSgaSnkYevEdFw')
-c_sheet = cords.sheet1
-cord = pd.DataFrame(c_sheet.get_all_records())
+    #import trait info spreadsheet to pandas
+    traits = gc.open_by_key('1yZCN3vkQWZLQea1xOvfAb4OzfM8zqN4tQn44obWT7m4')
+    t_sheet = traits.sheet1
+    trait = pd.DataFrame(t_sheet.get_all_records())
 
-#import allele spreadsheet to pandas
-alleles = gc.open_by_key('1SzXv3RQIim-eYhW4-OtVjveDd9Ph6QuXSTDTXqcN0ug')
-a_sheet = alleles.sheet1
-al = pd.DataFrame(a_sheet.get_all_records())
+    #merging data onto one database
+    flt = pd.merge(cord, al, copy=False, on='CODE')
 
-#import trait info spreadsheet to pandas
-traits = gc.open_by_key('1yZCN3vkQWZLQea1xOvfAb4OzfM8zqN4tQn44obWT7m4')
-t_sheet = traits.sheet1
-trait = pd.DataFrame(t_sheet.get_all_records())
+    res = pd.merge(flt, trait, copy=False, on='RS')
 
-print "retrieved sheets from google drive"
+    full = pd.merge(res, pop, copy=False, on='CODE')
 
-print "creating database..."
+    #converts cordinate and allele data to numpy array
+    array = flt.as_matrix()
+    return array
 
-#merging data onto one database
-flt = pd.merge(cord, al, copy=False, on='CODE')
+def render(number_points, smoothing, radius, array):
+    #takes numpy array and turns data into values to go on map
+    lat = [float(item[1]) for item in array] #isolates lat from the main numpy array
+    lon = [float(item[2]) for item in array] #isolates lon
+    a = [float(item[3]) for item in array] #isolates the allele freq
 
-res = pd.merge(flt, trait, copy=False, on='RS')
+    real_zip = (zip(lat, lon)) #creates list that only has lat and lon to be used in function getPoints
+    real = np.array(real_zip) #converts the list to numpy array so the lat and lon can be accessed
+    old_data = (zip(lat,lon,a))
+    dataset = np.array(real_zip)
 
-full = pd.merge(res, pop, copy=False, on='CODE')
+    new_data = main(dataset,lat,lon,a,smoothing,number_points) #second to last arg is smoothing, arg after that is number of points in between
 
-#converts cordinate and allele data to numpy array
-array = flt.as_matrix()
+    data_cord = np.concatenate((old_data,new_data)) #merges the original set of cordinates and the interpolated cordinates
 
-print "database finished and turned into array"
-print "getting cordinates..."
+    #replace stamentoner with whatever tileset is best (custom ones can be made)
+    m = folium.Map([48., 5.], tiles='stamentoner',control_scale = True, zoom_start=1)
 
-#takes numpy array and turns data into values to go on map
-lat = [float(item[1]) for item in array]
-lon = [float(item[2]) for item in array]
-a = [float(item[3]) for item in array]
+    #play around with radius value to see which one is best, and gradient changes heatmap colors
+    m.add_child(plugins.HeatMap(data_cord, radius = radius, gradient={.1: 'blue', .2: 'green', .3: 'orange', .5: 'red', .7: 'white'}))
 
-real_zip = (zip(lat, lon)) #creates list that only has lat and lon to be used in function getPoints
-real = np.array(real_zip) #converts the list to numpy array so the lat and lon can be accessed
+    m.save('map.html') #saves map as html file
 
-lat_lon = (zip(lat,lon))
+if __name__ == "__main__":
+    number_points = input("How many points do you want in between? ")
+    if number_points > 25:
+        print "That's too big of a number!"
+    else:
+        print "Alrighty!"
 
-old_data = (zip(lat,lon,a))
-dataset = np.array(lat_lon)
-print dataset
+    smoothing = input("What would you like the smoothing to be? ")
 
-coord = (100.11,100.11)
+    print "Sure!"
+
+    radius = input("What do you want the radius to be? ")
+
+    print "No problem!"
+
+    print "Wait a sec..."
+
+    ar = getData()
+    render(number_points, smoothing, radius, ar)
 
 
-new_data = getPoints(dataset,lat,lon,a,10,3) #second to last arg is smoothing, arg after that is number of points in between
+    print "Before I can give you the map you must enter your credit card number!"
+    credit_card = input("Enter credit card number here: ")
+    print "Hahaha just kidding! But if you actually entered your credit card then thanks!"
+    print "Let's continue shall we?"
 
-data_cord = np.concatenate((old_data,new_data)) #merges the original set of cordinates and the interpolated cordinates
-
-print "got cordinates"
-
-print "creating map..."
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-dat1 = np.array(old_data)
-dat = dat1[dat1[:, 1].argsort()]
-print dat
-ax.plot(dat[0], dat[1], dat[2], label='parametric curve')
-
-plt.show()
-
-#replace stamentoner with whatever tileset is best (custom ones can be made)
-m = folium.Map([48., 5.], tiles='stamentoner',control_scale = True, zoom_start=1)
-
-#play around with radius value to see which one is best, and gradient changes heatmap colors
-m.add_child(plugins.HeatMap(data_cord, radius = 10, gradient={.2: 'blue', .4: 'yellow', .6: 'orange', .8: 'red', 1: 'white'}))
-
-print "created map"
-
-print "saving map..."
-m.save('map.html') #saves map as html file
-print "map saved"
-
-print "kind of created by Connor Caldwell"
 
 endTime = time.time()-start
 
-if endTime < 2:
-    print '-----This program ran in', endTime, 'seconds! That was super fast!-----' #got bored over the weekend so I made this :)
-else:
-    print '-----This program ran in', endTime, 'seconds. That was really slow.-----'
+print 'Finished the map in', endTime, 'seconds!'
+
+print "kind of created by Connor Caldwell"
 
 """
 _________
